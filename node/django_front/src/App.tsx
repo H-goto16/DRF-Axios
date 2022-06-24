7380eb53ce151310e0a4a6d2dbfd2fe045858d12
H-goto16 <h.goto.engineer@gmail.com>
2022-06-24 14:16:16 +0900

    indent changed

--- node/django_front/src/App.tsx
+++ node/django_front/src/App.tsx
@@ -7,23 +7,23 @@ name: string;
 price: number
 }
 export const App = () => {
-const urlAPI = "http://localhost:8000/products/products/";
-const [datas, setDatas] = useState([]);
-console.log(axios.defaults.baseURL)
-useEffect( () => {
-  axios.get(urlAPI).then((res)=> {
-    setDatas(res.data);
-  })
-},[])
-console.log(datas)
-return (
-  <div className="App">
-    <h1>Axios Test</h1>
-    <div>
-      {datas.map((data: Data) => (
-        <div>{data.name} : {data.price}</div>
-      ))}
+  const urlAPI = "http://localhost:8000/products/products/";
+  const [datas, setDatas] = useState([]);
+  console.log(axios.defaults.baseURL)
+  useEffect( () => {
+    axios.get(urlAPI).then((res)=> {
+      setDatas(res.data);
+    })
+  },[])
+  console.log(datas)
+  return (
+    <div className="App">
+      <h1>Axios Test</h1>
+      <div>
+        {datas.map((data: Data) => (
+          <div>{data.name} : {data.price}</div>
+        ))}
+      </div>
     </div>
-  </div>
-);}
+  );}
 export default App;
