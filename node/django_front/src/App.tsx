5b318057c8482f373e4d677ff7fe3cc79b94aa10
H-goto16 <h.goto.engineer@gmail.com>
2022-06-24 22:17:34 +0900

    consider axios post

--- node/django_front/src/App.tsx
+++ node/django_front/src/App.tsx
@@ -7,6 +7,13 @@ type Data = {
   price: number
 }
 export const App = () => {
+  const [inputValue, setInputvalue] = useState("");
+  const [text, setText] = useState<any>([]);
+
+  type Text = {
+    inputValue :string;
+  }
+
   const urlAPI = "http://localhost:8000/products/products/";
   const [datas, setDatas] = useState([]);
   console.log(axios.defaults.baseURL)
@@ -16,14 +23,41 @@ export const App = () => {
     })
   },[])
   console.log(datas)
+
+  function handleChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
+    console.log(e.target.value)
+    setInputvalue(e.target.value);
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const newText :any = {
+      inputValue:inputValue,
+    };
+    setInputvalue("")
+    setText([newText,...text])
+    console.log(text)
+  }
+
   return (
-    <div className="App">
-      <h1>Axios Test</h1>
+    <main className="container">
+      <p className="title">簡易掲示板</p> 
+      <p className="text">こちらは簡易掲示板のサイトとなります。ログインを行うことで投稿名が自動的に入力されます。登録していない場合はAnonumousUserと表示されます。</p>
+      <p><a href="" className="link">サインアップ</a></p>
+      <p><a href="" className="link">ログイン</a></p>
+      <form onSubmit={(e) => {handleSubmit(e)}} action="" method="post">
+        <textarea name="comment"onChange={(e) => handleChange(e)} className="form-control">
+        </textarea>
+        <input type="submit" value="送信" className="comit" />
+      </form>
       <div>
+
         {datas.map((data: Data) => (
-          <div>{data.name} : {data.price}</div>
-        ))}
+          <div className='dis'>{data.name} : {data.price}</div>
+          ))}
       </div>
-    </div>
-  );}
+      &nbsp;
+    </main>
+    
+    );}
 export default App;
